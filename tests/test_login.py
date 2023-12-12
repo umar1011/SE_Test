8eb66a6b5c6f24900662aa6a3e18b51d799cfd62
umar1011 <75946577+umar1011@users.noreply.github.com>
2023-12-12 17:32:00 +0500

    Update test_login.py

--- tests/test_login.py
+++ tests/test_login.py
@@ -12,7 +12,7 @@ def test_login_with_valid_credentials(browser, username, password):
     assert browser.current_url == "https://www.saucedemo.com/inventory.html"
 
 
-@pytest.mark.parametrize("username, password", [("userwrong", "secret_sauce"), ("valid_username", "invalid_password")])
+@pytest.mark.parametrize("username, password", [("userwrong", "secret_sauce"), ("valid_username", "invalid_password"), ("userwrong", "invalid_password"),('', '')])
 def test_login_with_invalid_credentials(browser, username, password):
     login_page = LoginPage(browser)
     login_page.enter_username(username)
